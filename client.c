003760f1e3f8061061ec9d03aa3e005cf3ce4aaf
Abderrahmane Ababachir <aababach@e1r5p11.1337.ma>
2022-11-12 18:17:28 +0100

    school

--- client.c
+++ client.c
@@ -4,116 +4,111 @@
 #include <unistd.h>
 #include <sys/proc.h>
 
-void    putstr(char *str)
+void	putstr(char *str)
 {
-    int i;
-    int a;
+	int	i;
+	int	a;
 
-    i = 0;
-    while (str[i])
-    {
-        write(1, &str[i],1);
-        i++;
-    }
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
 
-void    validpid(char *pid)
+void	validpid(char *pid)
 {
-    int i = 0;
-    while (pid[i])
-    {
-        if (pid[i] < '0' || pid[i] > '9')
-        {
-            putstr("UNVALID PID");
-            exit(0);
-        }
-        i++;
-    }
+	int	i = 0;
+	while (pid[i])
+	{
+		if (pid[i] < '0' || pid[i] > '9')
+		{
+			putstr("UNVALID PID");
+			exit(0);
+		}
+		i++;
+	}
 }
 
-void    putnbr(int n)
+void	putnbr(int n)
 {
-    char a;
+	char	a;
 
-    a = 0;
-    if (n > 9)
-        putnbr(n / 10);
-    a = n % 10 + '0';
-    write(1, &a,1);
+	a = 0;
+	if (n > 9)
+		putnbr(n / 10);
+	a = n % 10 + '0';
+	write(1, &a, 1);
 }
 
 int ft_atoi(char *str)
 {
-    int i;
-    int l;
-    int s;
-    int n;
+	int	i;
+	int	l;
+	int	s;
+	int	n;
 
-    i = 0;
-    s = 1;
-    l = 0;
-    n = 0;
-    if (str[0] == '-')
-    {
-        s = -1;
-        i = 1;
-    }
-    while (str[l])
-        l++;
-    if (s == -1)
-        l--;
-    while (str[i])
-    {
-        n = (n * 10) + (str[i] - '0');
-        i++;
-        l--;
-    }
-    return (n * s);
-}
-void    test(int a)
-{
-    a = 0;
+	i = 0;
+	s = 1;
+	l = 0;
+	n = 0;
+	if (str[0] == '-')
+	{
+		s = -1;
+		i = 1;
+	}
+	while (str[l])
+		l++;
+	if (s == -1)
+		l--;
+	while (str[i])
+	{
+		n = (n * 10) + (str[i] - '0');
+		i++;
+		l--;
+	}
+	return (n * s);
 }
 
-int main(int argv, char **argc)
+void	test(int a)
 {
-    char    *pid;
-    char    *str;
-    int val;
-    int bitcount;
+	a = 0;
+}
 
-    // printf("this is my pid %d\n",getpid());
-    // if (argv <= 3)
-    //     return (0);
-    struct sigaction sa;
-    sa.__sigaction_u.__sa_handler = test;
-    sigaction(SIGUSR1,&sa,NULL);
-    val = 0;
-    bitcount = 0;
-    pid = argc[1];
-    str = (argc[2]);
-    validpid(pid);
-    int i = 0;
-    int s_count = 0;
-    while (str[i])
-    {
-       bitcount = 7;
-        // printf("this is i %d\n",i);
-        while (bitcount >= 0)
-        {
-            val = (str[i]>>bitcount) % 2;
-            if (val < 0)
-                val *= -1;
-            // printf("this is the number without shift %d\n", (str[i]));
-            // printf("this is the number %d\n", val);
-            // printf("this is the signal %d\n", val + 30);
-            kill(ft_atoi(pid),val + 30);
-            s_count++;
-            pause();
-            // kill(ft_atoi(pid),9);
-            usleep(100);
-            bitcount--;
-        }
-        i++;
-    }
+int	main(int argv, char **argc)
+{
+	char	*pid;
+	char	*str;
+	int	val;
+	int	bitcount;
+
+	if (argv != 3)
+		return (0);
+	struct sigaction sa;
+	sa.__sigaction_u.__sa_handler = test;
+	sigaction(SIGUSR1, &sa, NULL);
+	val = 0;
+	bitcount = 0;
+	pid = argc[1];
+	str = (argc[2]);
+	validpid(pid);
+	int i = 0;
+	int s_count = 0;
+	while (str[i])
+	{
+		bitcount = 7;
+		while (bitcount >= 0)
+		{
+			val = (str[i]>>bitcount) % 2;
+			if (val < 0)
+				val *= -1;
+			kill(ft_atoi(pid), val + 30);
+			s_count++;
+			pause();
+			usleep(100);
+			bitcount--;
+		}
+		i++;
+	}
 }
--- server.c
+++ server.c
@@ -1,71 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 18:10:34 by aababach          #+#    #+#             */
+/*   Updated: 2022/11/12 18:14:25 by aababach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/proc.h>
 
-char    str;
-int     b_count;
+char	g_str;
 
-void    putstr(char *s)
+void	putstr(char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        write(1, &s[i],1);
-        i++;
-    }
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
 }
 
-void    putnbr(int n)
+void	putnbr(int n)
 {
-    char a;
+	char	a;
 
-    a = 0;
-    if (n > 9)
-        putnbr(n / 10);
-    a = n % 10 + '0';
-    write(1, &a,1);
+	a = 0;
+	if (n > 9)
+		putnbr(n / 10);
+	a = n % 10 + '0';
+	write(1, &a, 1);
 }
 
-void    test(int sig, siginfo_t *inf, void *c)
+void	test(int sig, siginfo_t *inf, void *c)
 {
-    static int count = 0;
+	static int	count = 0;
 
-    // printf("this is the source pid %d\n",inf->si_pid);
-    str = (str * 2) + sig - 30;
-    b_count++;
-    // fprintf(stderr, "\nthis is the signal %d\n",sig);
-    // fprintf(stderr,"\nthis is the value %d ",str);
-    count++;
-    if (count == 8)
-    {
-        count = 0;
-        // fprintf(stderr,"\nthis is the final value %d ",str);
-        write(1, &str, 1);
-        str = 0;
-    }
-    usleep(110);
-    kill(inf->si_pid, 30);
+	(void)c;
+	g_str = (g_str * 2) + sig - 30;
+	count++;
+	if (count == 8)
+	{
+		count = 0;
+		write(1, &g_str, 1);
+		g_str = 0;
+	}
+	usleep(115);
+	kill(inf->si_pid, 30);
 }
 
-int main()
+int	main(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_flags = SA_SIGINFO;
-    // sa.__sigaction_u.__sa_handler = &test;
-    sa.__sigaction_u.__sa_sigaction = &test;
-    // signal(SIGUSR1,test);
-    // signal(SIGUSR2,test);
-    sigaction(SIGUSR1,&sa,NULL);
-    sigaction(SIGUSR2,&sa,NULL);
-    putnbr(getpid());
-    while (1)
-    {
-        pause();
-        // printf("this is the b_count %d\n",b_count);
-    }
+	sa.sa_flags = SA_SIGINFO;
+	sa.__sigaction_u.__sa_sigaction = &test;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	putnbr(getpid());
+	while (1)
+		pause();
 }
