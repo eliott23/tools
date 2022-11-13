730e9bb5312ec525699b3f0f6399272212e565c2
Abderrahmane Ababachir <aababach@e1r5p11.1337.ma>
2022-11-13 13:25:59 +0100

    school

--- header.h
+++ header.h
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 13:25:15 by aababach          #+#    #+#             */
+/*   Updated: 2022/11/13 13:25:54 by aababach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HEADER_H
 # define HEADER_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/proc.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <signal.h>
+# include <unistd.h>
+# include <sys/proc.h>
 
 void	putstr(char *str);
 void	validpid(char *pid);
